65cd12871860c04aaed90b2d0f2e99f31beffea7
gorvelg <ggorvel@gmail.com>
2024-07-09 11:10:56 +0200

    STYLE : update home

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'colors': {
+        'primary': {
+          '200': 'rgba(0, 155, 147, 0.1)',
+          '300': '#d2e0e042',
+        },
+        'text': {
+          '800': '#083448',
+        },
+      },
+      fontFamily: {
+        display: 'Poppins'
+      }
+    },
   },
   plugins: [],
 }
-
