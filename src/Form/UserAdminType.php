d977c9c0a4a173ebf672556cbf0969782050862b
Guillaume Gorvel <ggorvel@gmail.com>
2024-12-07 15:31:06 +0100

    style(form user admin)

--- src/Form/UserAdminType.php
+++ src/Form/UserAdminType.php
@@ -6,7 +6,10 @@ use App\Entity\Home;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,14 +18,21 @@ class UserAdminType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe',
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
                 'required' => false,
                 'attr' => ['class' => 'form-control']
             ])
-            ->add('color')
-            ->add('email')
+            ->add('color', ColorType::class, [
+                'label' => 'Couleur',
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
             ->add('home', EntityType::class, [
                 'class' => Home::class,
                 'choice_label' => 'id',
