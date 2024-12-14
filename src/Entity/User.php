f521338c581d50bd414c40d0dd7500f676617e16
Guillaume Gorvel <ggorvel@gmail.com>
2024-12-14 18:52:47 +0100

    feat(User.php) unique email

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['email'], message: 'Cette adresse email est déjà utilisée')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -44,7 +44,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 30, nullable: true)]
     private ?string $color = null;
 
-    #[Assert\Unique(message: 'Cette adresse email est déjà utilisée.')]
     #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
